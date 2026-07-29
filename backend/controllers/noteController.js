35a54daab9d0463f84c9ce570cb6b967f399965b
trishyanigam <trishyanigam@gmail.com>
2026-07-29 16:32:55 +0530

    update and delete created

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -71,8 +71,78 @@ const getNoteById = async(req,res) =>{
     }
 };
 
+const updateNote = async (req,res)=>{
+    try{
+        const { title,content } = req.body;
+        if(!title || !content)
+        {
+            return res.status(400).json({
+                success: false,
+                message: "Title and content are required"
+            });
+        }
+
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id,
+        {
+            title,content
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    );
+        if(!updatedNote)
+        {
+            return res.status(404).json({
+                success: false,
+                message: "Note not found"
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Note updated successfully",
+            data: updatedNote
+        });
+
+    }
+    catch(err)
+    {
+        return res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
+
+const deleteNote = async(req,res) =>{
+    try
+    {
+        const deletedNote = await Note.findByIdAndDelete(req.params.id);
+        if(!deletedNote)
+        {
+            return res.status(404).json({
+                success: false,
+                message: "Note not found"
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Note deleted successfully"
+        });
+    }
+    catch(err)
+    {
+        return res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
+
 module.exports = {
     createNote,
     getAllNotes,
-    getNoteById
+    getNoteById,
+    updateNote,
+    deleteNote
 };
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createNote,getAllNotes, getNoteById } = require('../controllers/noteController');
+const { createNote,getAllNotes, getNoteById, updateNote, deleteNote } = require('../controllers/noteController');
 router.post('/',createNote);
 router.get('/',getAllNotes);
 router.get('/:id',getNoteById);
+router.put('/:id',updateNote);
+router.delete('/:id',deleteNote);
 module.exports = router;
